f9c754b102e880f6fb59f265134ddb6f9ae6b07f
Adam Kolb <adam.kolb9@gmail.com>
2021-08-30 15:56:42 -0400

    Added import maze and began breadth first search algorithm

---
+++ breadth_first.py
@@ -0,0 +1,56 @@
+from numpy.lib.function_base import trapz
+import load_maze
+
+def find_moves(data, location):
+    directions = [[-1,0], [1,0], [0, -1], [0, 1]]
+    moves = []
+    
+    for direction in directions:
+        move_location = [
+            location[0] + direction[0],
+            location[1] + direction[1]]
+        if move_location[0] < 0 or move_location[1] < 0:
+            continue
+        elif data[move_location[0]][move_location[1]] == 1:
+            moves.append(move_location)
+    return moves
+
+def main(location, bordered = False):
+    maze_data, end, start = load_maze.get_grid(location, True)
+    que = [start]
+    explored = []
+    solution_found = False
+    while True:
+        moves = find_moves(maze_data, que[0])
+        explored.append(que.pop(0))
+        for move in moves:
+            if move == end:
+                solution_found = True
+                break
+            if move in explored or move in que:
+                continue
+            else:
+                que.append(move)
+        if solution_found:
+            break
+
+    move_list = [end]
+    while True:
+        moves = find_moves(maze_data, move_list[-1])
+        min_index = len(explored) + 1
+        for move in moves:
+            if move == start:
+                move_list.append(start)
+                break
+            if move in explored:
+                min_index = min(min_index, explored.index(move))
+        move_list.append(explored[min_index])
+        if start in move_list:
+            break
+
+    return move_list[::-1]
+
+
+if __name__ == '__main__':
+   moves = main("Mazes/maze1.gif", True )
+   print(moves)
---
+++ load_maze.py
@@ -0,0 +1,157 @@
+from PIL import Image
+from time import sleep
+import numpy as np
+from numpy.core.numeric import outer
+
+def load_image(location):
+    image = Image.open(location)
+    image_data = np.asarray(image)
+
+    return image_data
+
+def remove_white_border(image_data):
+    i, j, k, z = 0, 0, 0, 0
+    rev_data = image_data[::-1]
+    output_data = []
+    for index, row in enumerate(image_data): #finds start of black border @ top
+        if 0 in row:
+            i = index
+            break
+    for index, row in enumerate(rev_data): #find end of black border @ bottom
+        if 0 in row:
+            j = index
+            break
+    for ii in range(len(image_data[0])): # find start of black border @ left
+        if 0 in image_data[:,ii]:
+            k = ii
+            break
+    for ii in range(1, len(image_data[0])): # find end of black border @ right
+        if 0 in image_data[:, len(image_data[0]) - ii]:
+            z = ii
+            break
+    
+    output_data = []
+    for index, row in enumerate(image_data[i:-j]):
+        output_data.append(row[k:-z+1]) # removes left and right white border
+
+    return output_data
+
+
+def find_smallest(colour, data):
+    smallest = 1000
+    current = 0
+    for row in data:
+        for i in row:
+            if i != colour:
+                if current < smallest and current != 0:
+                    smallest = current
+                current = 0
+                continue
+            else:
+                current += 1
+        if current < smallest and current != 0:
+            smallest = current
+        current = 0
+    return smallest
+
+def num_sections(data_length, yes, no):
+    sections = (data_length - no) // (no + yes) #finds how many sections there are of no and yes
+    sections *= 2 # multiplies by 2 to account for both yes and no
+    sections += 1 # adds 1 for iniital no
+    return int(sections)
+
+def create_grid(data, yes_row, yes_col, no_row, no_col):
+    test1 = len(data) - no_col
+    test1 = test1 % (yes_col + no_col)
+    test2 = len(data[0]) - no_row
+    test2 = test2 % (yes_row + no_row)
+
+    if test1 + test2 != 0:
+        print(f"Error, data length {len(data)} or {len(data[0])} not divisable")
+        return 0
+    sections_col = num_sections(len(data), yes_col, no_col)
+    sections_row = num_sections(len(data[0]), yes_row, no_row)
+
+    output_grid = []
+
+
+    for i in range(sections_row):
+        no_row_multipler = i // 2 + i % 2
+        yes_row_multipler = i // 2
+        ii = no_row_multipler * no_row + yes_row_multipler * yes_row
+        
+        output_grid.append([])
+        
+        for j in range(sections_col):
+            no_col_multipler = j // 2 + j % 2
+            yes_col_multipler = j // 2
+            jj = no_col_multipler * no_col + yes_col_multipler * yes_col
+
+            output_grid[-1].append(data[ii][jj])
+
+    return output_grid
+
+def find_start_end(data):
+    output = []
+    for index, i in enumerate(data[0]):
+        if i == 1:
+            output.append([0, index])
+            if len(output) == 2:
+                return output[0], output[1]
+    for index, i in enumerate(data[-1]):
+        if i == 1:
+            output.append(len(data) - 1, index)
+            if len(output) == 2:
+                return output[0], output[1]
+
+    for i in range(len(data)):
+        if data[i][0] == 1:
+            output.append([i, 0])
+        if data[i][-1] == 1:
+            output.append([i, len(data[0]) - 1])
+        if len(output) == 2:
+            return output[0], output[1]
+    print(f'Error, only found entrance {output}')
+    return 0
+
+def get_grid(input_location, white_border = False):
+    data = load_image(input_location)
+    if white_border:
+        data = remove_white_border(data)
+    white_row_smallest = find_smallest(1, data)
+    white_col_smallest = find_smallest(1, np.transpose(data))
+    black_row_smallest = find_smallest(0, data)
+    black_col_smallest = find_smallest(0, np.transpose(data))
+    maze_grid = create_grid(
+        data,
+        white_row_smallest,
+        white_col_smallest,
+        black_row_smallest,
+        black_col_smallest
+    )
+    start, end = find_start_end(maze_grid)
+
+    return maze_grid, start, end
+
+if __name__ == "__main__":
+    # data = load_image("Mazes/maze1.gif")
+    # data_no_border = remove_white_border(data)
+    # data_no_border_array = np.array(data_no_border)
+    # white_row_smallest = find_smallest(1, data_no_border)
+    # white_col_smallest = find_smallest(1, np.transpose(data_no_border))
+    # black_row_smallest = find_smallest(0, data_no_border)
+    # black_col_smallest = find_smallest(0, np.transpose(data_no_border))
+    # maze_grid = create_grid(
+    #     data_no_border_array,
+    #     white_row_smallest,
+    #     white_col_smallest,
+    #     black_row_smallest,
+    #     black_col_smallest
+    # )
+    # start, end = find_start_end(maze_grid)
+    # maze_grid_array = np.array(maze_grid)
+    # image = Image.fromarray(np.uint8(maze_grid_array * 255), "L")
+    # image.save('test.png')
+    grid, start, end = get_grid("Mazes/maze1.gif", True)
+    print(f"{start} {end}")
+    
