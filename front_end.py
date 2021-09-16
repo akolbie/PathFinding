5499379b6266b756745217584f5e8ab7b0628b74
Adam Kolb <adam.kolb9@gmail.com>
2021-09-15 23:13:57 -0400

    Finished front end.

--- front_end.py
+++ front_end.py
@@ -1,43 +1,97 @@
-import tkinter as tk
-from time import sleep
-import load_maze
+from pygame.locals import *
+import pygame 
+from math import floor
 import breadth_first
+from load_maze import get_grid
 
-class MainPage():
-    def __init__(self, input_location, output_location, white_border = False):
-        self.input_location = input_location
-        self.output_location = output_location
 
-        self.data, self.start, self.end = load_maze.get_grid(self.input_location, white_border)
-        self.root = tk.Tk()
-        self.run = tk.Button(self.root,command=self.show_explore, text='Run')
-        self.run.pack(side='top')
-        self.canvas = tk.Canvas(self.root)
-        self.canvas.pack(side='top')
-        self.explored, self.moves, _ = breadth_first.main(self.input_location, True)
-        self.show_grid()
+class App:
+ 
+    player = 0
+ 
+    def __init__(self, data, start, end, explored, path, block_width, block_height):
+        self.bw = block_width
+        self.bh = block_height
+        self.windowWidth = len(data[0]) * self.bw
+        self.windowHeight = len(data) * self.bh
+        self._running = True
+        self._display_surf = None
+        self._image_surf = None
+        self._block_surf = None
+        self.maze = Maze(data, start, end, explored, path, self.bw, self.bh)
+ 
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+        
+        pygame.display.set_caption('Pygame pythonspot.com example')
+        self._running = True
+        self._image_surf = pygame.image.load("Blocks/player.png").convert()
+        self._block_surf = pygame.image.load("Blocks/block.png").convert()
+        self._start_surf = pygame.image.load("Blocks/start.png").convert()
+        self._end_surf = pygame.image.load("Blocks/end.png").convert()
+        self._explored_surf = pygame.image.load("Blocks/explored.png").convert()
+        self._path_surf = pygame.image.load("Blocks/path.png").convert()
 
-        self.root.mainloop()
+ 
+    def on_event(self, event):
+        if event.type == QUIT:
+            self._running = False
+    
+    def on_render(self):
+        self._display_surf.fill((0,0,0))
+        self.maze.draw(self._display_surf, self._block_surf, self._start_surf, self._end_surf, self._explored_surf, self._path_surf)
+        pygame.display.flip()
+ 
+    def on_cleanup(self):
+        pygame.quit()
+ 
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
 
-    def show_grid(self):
-        self.cells = {}
-        for i in range(len(self.data)):
-            for j in range(len(self.data[0])):
-                b = tk.Button(self.canvas, text='',width=1,height=1)
-                if self.data[i][j] == 0:
-                    b.config({'background' : 'Black'})
-                else:
-                    b.config({'background' : 'White'})
-                b.grid(column=j, row =i )
-                self.cells[(i,j)] = b
+            self.on_render()
+        self.on_cleanup()
+      
 
-    def show_explore(self):
-        for step in self.explored:
-            if step in self.moves:
-                self.cells[(step[0],step[1])].config({'background' : "Blue"})
-            else:
-                self.cells[(step[0],step[1])].config({'background' : "Red"})
-            sleep(1)
+class Maze:
+    def __init__(self, data, start, end, explored, path, bw, bh):
+        self.M = len(data[0])
+        self.N = len(data)
 
-if __name__ == "__main__":
-    MainPage('Mazes/maze1.gif', 'BFS_complete_path.png', True)
+        self.maze = data[:]
+        self.start = start
+        self.end = end
+        self.explored = explored
+        self.path = path
+        self.counter = 0
+
+        self.bw = bw
+        self.bh = bh
+
+    def draw(self, display_surf, block_surf, start_surf, end_surf, explored_surf, path_surf):
+        self.counter += 1
+        steps = floor(self.counter / .1)
+
+        for row_index, row in enumerate(self.maze):
+            for i_index, i in enumerate(row):
+                if i == 1:
+                    display_surf.blit(block_surf,(i_index * self.bw, row_index * self.bh))
+        
+        display_surf.blit(start_surf,(self.start[1] * self.bh, self.start[0] * self.bw))
+        display_surf.blit(end_surf, (self.end[1] * self.bh, self.end[0] * self.bw))
+
+        for i in range(min(len(self.explored) - 1, steps)):
+            display_surf.blit(explored_surf,(self.explored[i][1] * self.bh, self.explored[i][0] * self.bw))
+
+        for j in range(min(max(0, steps - (len(self.explored) - 1)), len(self.path) - 1)):
+            display_surf.blit(path_surf, (self.path[j][1] * self.bh, self.path[j][0] * self.bw))
+
+
+if __name__ == "__main__" :
+    data, start, end = get_grid("Mazes/maze1.gif", True)
+    explored, path = breadth_first.breadth_first_main(data, start, end)
+    theApp = App(data, start, end, explored, path, 10, 10)
+    theApp.on_execute()
